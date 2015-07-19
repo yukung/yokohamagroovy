65102a82db9e8e056f2338e3d54f563424cc3d36
yukung <yukung.i@gmail.com>
2015-07-19 17:59:49 +0900

    Fix the package.

--- gradle-thoroughly-introduction/libraries-sample/src/main/java/org/yukung/yokohamagroovy/libraries/App.java
+++ gradle-thoroughly-introduction/libraries-sample/src/main/java/org/yukung/yokohamagroovy/libraries/App.java
@@ -1,4 +1,4 @@
-package demo;
+package org.yukung.yokohamagroovy.libraries;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;

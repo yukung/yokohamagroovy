769cc128d0b750af02553f26358dbb00e6ec2594
yukung <yukung.i@gmail.com>
2015-07-19 15:03:10 +0900

    Rename bootstrap class.

--- gradle-thoroughly-introduction/libraries-sample/src/test/java/demo/AppTests.java
+++ gradle-thoroughly-introduction/libraries-sample/src/test/java/demo/AppTests.java
@@ -7,12 +7,12 @@ import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = DemoApplication.class)
+@SpringApplicationConfiguration(classes = App.class)
 @WebAppConfiguration
-public class DemoApplicationTests {
+public class AppTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
 }

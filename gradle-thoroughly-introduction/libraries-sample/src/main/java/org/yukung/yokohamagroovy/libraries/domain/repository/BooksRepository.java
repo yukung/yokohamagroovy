484f540705079ee89dbd54829d4e840adac39dd3
yukung <yukung.i@gmail.com>
2015-08-30 17:15:49 +0900

    Add test case

---
+++ gradle-thoroughly-introduction/libraries-sample/src/main/java/org/yukung/yokohamagroovy/libraries/domain/repository/BooksRepository.java
@@ -0,0 +1,38 @@
+package org.yukung.yokohamagroovy.libraries.domain.repository;
+
+import org.jooq.DSLContext;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.yukung.yokohamagroovy.libraries.domain.tables.daos.BooksDao;
+import org.yukung.yokohamagroovy.libraries.domain.tables.pojos.Books;
+import org.yukung.yokohamagroovy.libraries.domain.tables.records.BooksRecord;
+
+import static org.yukung.yokohamagroovy.libraries.domain.tables.Books.BOOKS;
+
+/**
+ * @author yukung
+ */
+@Repository
+public class BooksRepository {
+
+    @Autowired
+    private DSLContext dsl;
+
+    public Books findOne(String isbn) {
+        return new BooksDao(dsl.configuration()).findById(isbn);
+    }
+
+    public Books save(Books books) {
+        BooksRecord booksRecord = dsl
+                .selectFrom(BOOKS)
+                .where(BOOKS.ISBN.eq(books.getIsbn()))
+                .fetchOne();
+        if (booksRecord == null) {
+            booksRecord = dsl.newRecord(BOOKS, books);
+        } else {
+            booksRecord.from(books);
+        }
+        booksRecord.store();
+        return booksRecord.into(Books.class);
+    }
+}

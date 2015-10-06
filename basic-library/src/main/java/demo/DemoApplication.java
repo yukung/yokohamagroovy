5f0513457c15ef4358746c5dd55ae264487cf2d5
yukung <yukung.i@gmail.com>
2015-10-06 16:19:53 +0900

    Add skeleton template files.

--- basic-library/src/main/java/demo/DemoApplication.java
+++ basic-library/src/main/java/demo/DemoApplication.java
@@ -2,11 +2,22 @@ package demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @SpringBootApplication
+@Controller
 public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @RequestMapping("/")
+    public String sayHello(@RequestParam(name = "name", defaultValue = "World") String name, Model model) {
+        model.addAttribute("message", "Hello, " + name + "!");
+        return "index";
     }
 }

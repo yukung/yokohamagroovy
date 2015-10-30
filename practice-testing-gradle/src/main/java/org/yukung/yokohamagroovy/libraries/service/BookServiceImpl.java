22fd84cd06c86b3a760f145d687917ee592af353
yukung <yukung.i@gmail.com>
2015-10-30 18:00:13 +0900

    Implement create method for Book.

--- practice-testing-gradle/src/main/java/org/yukung/yokohamagroovy/libraries/service/BookServiceImpl.java
+++ practice-testing-gradle/src/main/java/org/yukung/yokohamagroovy/libraries/service/BookServiceImpl.java
@@ -1,14 +1,22 @@
 package org.yukung.yokohamagroovy.libraries.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.yukung.yokohamagroovy.libraries.entity.Book;
+import org.yukung.yokohamagroovy.libraries.repository.BooksRepository;
 
 /**
  * @author yukung
  */
+@Service
 public class BookServiceImpl implements BookService {
+
+    @Autowired
+    private BooksRepository repository;
+
     @Override
     public Book create(Book book) {
-        return null;
+        return repository.save(book);
     }
 
     @Override

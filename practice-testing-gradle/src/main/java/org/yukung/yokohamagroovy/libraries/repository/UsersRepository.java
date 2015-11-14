1a1fd9b3a2003ed60021bd78fc5ae35b07670bfe
yukung <yukung.i@gmail.com>
2015-11-14 20:07:04 +0900

    When the INSERT, modified to use SimpleJdbcInsert.

--- practice-testing-gradle/src/main/java/org/yukung/yokohamagroovy/libraries/repository/UsersRepository.java
+++ practice-testing-gradle/src/main/java/org/yukung/yokohamagroovy/libraries/repository/UsersRepository.java
@@ -3,14 +3,16 @@ package org.yukung.yokohamagroovy.libraries.repository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
-import org.springframework.jdbc.support.GeneratedKeyHolder;
-import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 import org.yukung.yokohamagroovy.libraries.entity.User;
+
+import javax.annotation.PostConstruct;
 
 /**
  * @author yukung
@@ -21,20 +23,29 @@ public class UsersRepository {
     @Autowired
     private NamedParameterJdbcTemplate jdbcTemplate;
 
+    private SimpleJdbcInsert insert;
+
+    @PostConstruct
+    void init() {
+        insert = new SimpleJdbcInsert((JdbcTemplate) jdbcTemplate.getJdbcOperations())
+                .withTableName("users")
+                .usingGeneratedKeyColumns("user_id");
+    }
+
     public User save(User user) {
         SqlParameterSource param = new BeanPropertySqlParameterSource(user);
         if (user.getUserId() == null) {
-            KeyHolder keyHolder = new GeneratedKeyHolder();
-            jdbcTemplate.update(
-                    "INSERT INTO users (user_name, user_address, phone_number, email_address, other_user_details) " +
-                            "VALUES (:userName, :userAddress, :phoneNumber, :emailAddress, :otherUserDetails)",
-                    param, keyHolder);
-            user.setUserId(keyHolder.getKey().longValue());
+            Number key = insert.executeAndReturnKey(param);
+            user.setUserId(key.longValue());
             return user;
         } else {
             jdbcTemplate.update(
                     "UPDATE users " +
-                            "SET user_name = :userName, user_address = :userAddress, phone_number = :phoneNumber, email_address = :emailAddress, other_user_details = :otherUserDetails " +
+                            "SET user_name = :userName," +
+                            " user_address = :userAddress," +
+                            " phone_number = :phoneNumber," +
+                            " email_address = :emailAddress," +
+                            " other_user_details = :otherUserDetails " +
                             "WHERE user_id = :userId",
                     param);
             return user;

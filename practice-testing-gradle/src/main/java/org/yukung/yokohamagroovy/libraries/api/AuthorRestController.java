9d1fc6c4ef1257d29442dfd5ab33b376da5e911e
yukung <yukung.i@gmail.com>
2015-12-14 21:24:57 +0900

    Refactor.

--- practice-testing-gradle/src/main/java/org/yukung/yokohamagroovy/libraries/api/AuthorRestController.java
+++ practice-testing-gradle/src/main/java/org/yukung/yokohamagroovy/libraries/api/AuthorRestController.java
@@ -15,29 +15,29 @@ import org.yukung.yokohamagroovy.libraries.service.author.AuthorService;
  * @author yukung
  */
 @RestController
-@RequestMapping("api")
+@RequestMapping("api/authors")
 public class AuthorRestController {
 
     @Autowired
     private AuthorService authorService;
 
-    @RequestMapping(method = RequestMethod.POST, path = "authors")
+    @RequestMapping(method = RequestMethod.POST)
     public Author postAuthors(@RequestBody Author author) {
         return authorService.create(author);
     }
 
-    @RequestMapping(method = RequestMethod.GET, path = "authors/{id}")
+    @RequestMapping(method = RequestMethod.GET, path = "{id}")
     public Author getAuthors(@PathVariable("id") Long authorId) {
         return authorService.find(authorId);
     }
 
-    @RequestMapping(method = RequestMethod.PUT, path = "authors/{id}")
+    @RequestMapping(method = RequestMethod.PUT, path = "{id}")
     @ResponseStatus(HttpStatus.CREATED)
     public void putAuthors(@PathVariable("id") Long authorId, @RequestBody Author author) {
         authorService.update(author);
     }
 
-    @RequestMapping(method = RequestMethod.DELETE, path = "authors/{id}")
+    @RequestMapping(method = RequestMethod.DELETE, path = "{id}")
     public void deleteAuthors(@PathVariable("id") Long authorId) {
         authorService.delete(authorId);
     }

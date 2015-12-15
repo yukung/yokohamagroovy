b008a172764c0299ebf63106c069f11f17c336ed
yukung <yukung.i@gmail.com>
2015-12-15 21:26:10 +0900

    Add test case.

---
+++ practice-testing-gradle/src/main/java/org/yukung/yokohamagroovy/libraries/api/UserRestController.java
@@ -0,0 +1,12 @@
+package org.yukung.yokohamagroovy.libraries.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author yukung
+ */
+@RestController
+@RequestMapping("/api/users")
+public class UserRestController {
+}
---
+++ practice-testing-gradle/src/test/java/org/yukung/yokohamagroovy/libraries/api/UserRestControllerTest.java
@@ -0,0 +1,13 @@
+package org.yukung.yokohamagroovy.libraries.api;
+
+import org.junit.Before;
+
+/**
+ * @author yukung
+ */
+public class UserRestControllerTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+}

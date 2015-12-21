74248babad64f6e8edb6c223fdfb1c619d94bcac
yukung <yukung.i@gmail.com>
2015-12-21 21:52:46 +0900

    Implement put handler for User.

--- practice-testing-gradle/src/main/java/org/yukung/yokohamagroovy/libraries/api/UserRestController.java
+++ practice-testing-gradle/src/main/java/org/yukung/yokohamagroovy/libraries/api/UserRestController.java
@@ -1,10 +1,12 @@
 package org.yukung.yokohamagroovy.libraries.api;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.yukung.yokohamagroovy.libraries.entity.User;
 import org.yukung.yokohamagroovy.libraries.service.user.UserService;
@@ -27,5 +29,11 @@ public class UserRestController {
     @RequestMapping(method = RequestMethod.GET, path = "{id}")
     public User getUsers(@PathVariable("id") Long userId) {
         return userService.find(userId);
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, path = "{id}")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void putUsers(@PathVariable("id") Long userId, @RequestBody User user) {
+        userService.update(user);
     }
 }

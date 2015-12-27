9252d35f311644e1861faee3c2818b8b498b3f84
yukung <yukung.i@gmail.com>
2015-12-27 22:05:28 +0900

    Implement create handler for Book.

--- practice-testing-gradle/src/main/java/org/yukung/yokohamagroovy/libraries/api/BookRestController.java
+++ practice-testing-gradle/src/main/java/org/yukung/yokohamagroovy/libraries/api/BookRestController.java
@@ -1,7 +1,25 @@
 package org.yukung.yokohamagroovy.libraries.api;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.yukung.yokohamagroovy.libraries.entity.Book;
+import org.yukung.yokohamagroovy.libraries.service.book.BookService;
+
 /**
  * @author yukung
  */
+@RestController
+@RequestMapping("/api/books")
 public class BookRestController {
+
+    @Autowired
+    private BookService bookService;
+
+    @RequestMapping(method = RequestMethod.POST)
+    public Book postUsers(@RequestBody Book book) {
+        return bookService.create(book);
+    }
 }

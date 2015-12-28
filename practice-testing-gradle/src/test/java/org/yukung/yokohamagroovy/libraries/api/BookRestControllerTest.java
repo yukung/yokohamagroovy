0a94f87245f0e8529fa3904901c73826b516e454
yukung <yukung.i@gmail.com>
2015-12-28 22:07:51 +0900

    Add test case.

--- practice-testing-gradle/src/test/java/org/yukung/yokohamagroovy/libraries/api/BookRestControllerTest.java
+++ practice-testing-gradle/src/test/java/org/yukung/yokohamagroovy/libraries/api/BookRestControllerTest.java
@@ -28,6 +28,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @RunWith(MockitoJUnitRunner.class)
 public class BookRestControllerTest {
 
+    public static final String ISBN = "123456789";
     private MockMvc mockMvc;
 
     @InjectMocks
@@ -64,5 +65,27 @@ public class BookRestControllerTest {
         assertThat(argument.getIsbn(), is("123456789"));
         assertThat(argument.getBookTitle(), is("test"));
         assertThat(argument.getDateOfPublication(), is(LocalDate.of(2000, 1, 1)));
+    }
+
+    @Test
+    public void testGetBooks() throws Exception {
+        // SetUp
+        Book book = new Book(ISBN, "test", LocalDate.of(2000, 1, 1));
+        when(bookService.create(book)).thenReturn(book);
+        when(bookService.find(ISBN)).thenReturn(book);
+
+        // Exercise&Verify
+        mockMvc.perform(get("/api/books/" + ISBN)
+                .contentType(APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(APPLICATION_JSON_UTF8))
+                .andExpect(content().json(mapper.writeValueAsString(book)));
+        ArgumentCaptor<String> captor = ArgumentCaptor.forClass(String.class);
+        verify(bookService, times(1)).find(captor.capture());
+        verifyNoMoreInteractions(bookService);
+
+        String argument = captor.getValue();
+        assertThat(argument, is(notNullValue()));
+        assertThat(argument, is(ISBN));
     }
 }

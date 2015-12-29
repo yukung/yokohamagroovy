280de65dc31f29b938b7e244f94a8aef9d92fc13
yukung <yukung.i@gmail.com>
2015-12-29 22:09:19 +0900

    Implement get handler for Book.

--- practice-testing-gradle/src/main/java/org/yukung/yokohamagroovy/libraries/api/BookRestController.java
+++ practice-testing-gradle/src/main/java/org/yukung/yokohamagroovy/libraries/api/BookRestController.java
@@ -1,6 +1,7 @@
 package org.yukung.yokohamagroovy.libraries.api;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -21,5 +22,10 @@ public class BookRestController {
     @RequestMapping(method = RequestMethod.POST)
     public Book postUsers(@RequestBody Book book) {
         return bookService.create(book);
+    }
+
+    @RequestMapping(method = RequestMethod.GET, path = "{id}")
+    public Book getBooks(@PathVariable("id") String isbn) {
+        return bookService.find(isbn);
     }
 }

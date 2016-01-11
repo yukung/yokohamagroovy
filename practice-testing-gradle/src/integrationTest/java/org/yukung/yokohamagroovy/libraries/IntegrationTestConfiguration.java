9ca620cba32c496b5a81fe2ee86d57db40b6c6ab
yukung <yukung.i@gmail.com>
2016-01-11 10:07:17 +0900

    Add the fixtures did not exist.

---
+++ practice-testing-gradle/src/integrationTest/java/org/yukung/yokohamagroovy/libraries/IntegrationTestConfiguration.java
@@ -0,0 +1,21 @@
+package org.yukung.yokohamagroovy.libraries;
+
+import jp.classmethod.testing.database.DataSourceDbUnitTester;
+import jp.classmethod.testing.database.DbUnitTester;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+/**
+ * @author yukung
+ */
+@Configuration
+public class IntegrationTestConfiguration {
+
+    @Bean
+    DbUnitTester dbUnitTester(DataSource dataSource) {
+        return DataSourceDbUnitTester.forDataSource(dataSource).schema("PUBLIC").create();
+//        return new DataSourceDbUnitTester(dataSource);    // これは schema がデフォルトで動く
+    }
+}
